2a6129e10e79b2e1dad01fd9b3ec082d40c04bce
Gabriel Theodoropoulos <gabriel@Gabriel-Theodoropouloss-Mac-mini.local>
2012-09-04 22:06:44 +0300

    Initial Commit

---
+++ ThreadingTestApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ThreadingTestApp
+//
+//  Created by Gabriel Theodoropoulos on 9/4/12.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ThreadingTestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ThreadingTestApp
+//
+//  Created by Gabriel Theodoropoulos on 9/4/12.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
